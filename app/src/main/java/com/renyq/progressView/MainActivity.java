f5fb8781936d4a92de9d47713687160b0b2fad83
renyq <renyq@tfedu.net>
2016-12-26 15:30:33 +0800

    Initial commit

---
+++ app/src/main/java/com/renyq/progressView/MainActivity.java
@@ -0,0 +1,17 @@
+package com.renyq.progressView;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
